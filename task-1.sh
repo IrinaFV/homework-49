3c66e823da7e1487e36139df9820941c492a90db
Irina Fedchenko <irina.fedchenko.00@gmail.com>
2023-05-11 14:33:03 +0600

    Написала команды, необходимые для создания директорий и файлов в командной строке.

---
+++ task-1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd ~
+mkdir projects
+cd projects
+mkdir my_project
+cd my_project
+mkdir directory1
+cd directory1
+mkdir subdirectory1
+cd ..
+mkdir directory2
+echo "Hello from JS" > hello.txt
+mkdir directory3
+mkdir subdirectory2
